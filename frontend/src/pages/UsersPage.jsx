13a723774a66599cfd6808f897fd8b860ca3662d
sumit-timilsina <mr.sumittimilsina@gmail.com>
2025-04-19 17:07:16 -0400

    users

---
+++ frontend/src/pages/UsersPage.jsx
@@ -0,0 +1,36 @@
+import { useChatStore } from "../store/useChatStore";
+import { useMediaQuery } from "react-responsive";
+
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
+
+const UsersPage = () => {
+  const { selectedUser } = useChatStore();
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  const renderContent = () => {
+    if (isMobile) {
+      if (!selectedUser) return <Sidebar />;
+      return <ChatContainer />;
+    }
+
+    return (
+      <div className="flex h-full rounded-lg overflow-hidden">
+        <Sidebar />
+      </div>
+    );
+  };
+
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          {renderContent()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UsersPage;
