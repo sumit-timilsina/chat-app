84039774de45bef02195ab1d151dbf6afde0cee4
sumit-timilsina <mr.sumittimilsina@gmail.com>
2025-04-19 23:02:02 -0400

    now

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,14 +8,12 @@ const Navbar = () => {
   return (
     <header
       className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
-    backdrop-blur-lg"
+    backdrop-blur-lg "
     >
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link 
-              onClick={() => setSelectedUser(null)}
-            to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useChatStore } from "../store/useChatStore";
+import { ArrowLeft } from "lucide-react";
 
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
@@ -9,16 +10,40 @@ const HomePage = () => {
 
   return (
     <div className="h-screen bg-base-200">
+      <div className="lg:hidden p-3">
+        <button
+          className="flex items-center gap-2 text-sm text-blue-500">
+          onClick={() => setSelectedUser(null)}
+          <ArrowLeft size={18} />
+        </button>
+      </div>
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
+            {/* Mobile: Show Sidebar if no user selected */}
+            {(!selectedUser) ? (
+              <div className="w-full block lg:hidden">
+                <Sidebar />
+              </div>
+            ) : (
+              // Mobile: Show ChatContainer if user is selected
+              <div className="flex-1 block lg:hidden">
+                <ChatContainer />
+              </div>
+            )}
 
-            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            {/* Desktop: Always show Sidebar and ChatContainer side by side */}
+            <div className="hidden lg:flex h-full w-full">
+              <Sidebar />
+              <div className="flex-1">
+                {selectedUser ? <ChatContainer /> : <NoChatSelected />}
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default HomePage;
